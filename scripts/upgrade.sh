f9566ba25d9426cae92217eea279d527efdb4275
henry1992007 <476679846@qq.com>
2023-04-07 17:56:46 +0800

    Update upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# kill existing
-pid=$(ps aux | grep 'ChatGPT-Next-Web' | grep -v 'grep' | head -n 1 | awk '{print $2}')
-
-if [[ -n "$pid" ]]; then
-	sudo kill $pid
-fi
-
+# upgrade
 cd /home/ubuntu/ChatGPT-Next-Web/
 git pull origin main
 yarn install
 yarn build
+
+# kill existing
+pid=$(ps aux | grep 'ChatGPT-Next-Web' | grep -v 'grep' | head -n 1 | awk '{print $2}')
+if [[ -n "$pid" ]]; then
+	sudo kill $pid
+fi
+
+# restart
 bash /home/ubuntu/launchGpt.sh
